aff38949f489b88dc7d6e6b357197fd68c639e6a
andrezlucas <andrepereiradv@gmail.com>
2025-10-19 22:00:25 -0300

    Banco

---
+++ src/usuario/entity/usuario.entity.ts
@@ -0,0 +1,17 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity()
+export class Usuario{
+   @PrimaryGeneratedColumn()
+   id: number;
+
+
+   @Column({
+       length:500
+   })
+   nome: string;
+
+
+   @Column('int')
+   idade: number;}
